4153ab24b565c0df261df650438dc0d7c849feb4
Dev Oza <55236505+Devojha408@users.noreply.github.com>
2024-07-03 07:58:27 +0000

    new client build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const path = require('path');
 const connectDB = require('./config/db');
 const coffeeShopRoutes = require('./routes/coffeeShopRoutes');
 
@@ -14,20 +13,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors()); // This allows requests from all origins
 app.use(bodyParser.json());
 
 // Routes
 app.use('/api/coffeeshops', coffeeShopRoutes);
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 
